d297dfca84b5c1190759af3c01d111ad7dadd0f3
Delanie Perez <delanieperez11@gmail.com>
2018-05-15 22:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number){
   var newNumber = [];
   var i = 0;
-  do {newNumber.push("I love the Beatles!"); i++;
+  do {newNumber.push("I love the Beatles!"); 
+    i++;
   } while (i < 15);
   return newNumber;
 }
